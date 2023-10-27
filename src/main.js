b4083a404d633c7dc93a3d98b4e3fddb7f3b591b
NicoPaint <felipepinve@gmail.com>
2023-10-27 08:36:51 -0500

    Se creo la funcionalidad de traer los generos (categorias) de las peliculas de la API y mostralos en la pagina web

--- src/main.js
+++ src/main.js
@@ -32,4 +32,35 @@ async function getTrendingMovies(){
     });
 }
 
+//esta funcion se usa para traer las categorias (generos) de las peliculas y mostrarlas en la sección de categories, es asincrona porque se va a consumir una API.
+async function getCategoriesMovies(){
+    //se hace la solicitud GET con fetch para traer el objeto de generos de peliculas.
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=${API_KEY}`);  //API_KEY esta oculta en otro archivo, usa tu propia API_KEY.
+    const data = await res.json();
+
+    const categories = data.genres; //categories es el objeto de generos. tiene una total de 19 elementos.
+    
+    //se itera por cada elemento en categories para sacar la informacion de cada una de los generos (id y nombre)
+    categories.forEach(category => {
+        //se selecciona el contenedor de toda la lista de categorias del HTML por su clase 
+        const categoriesMoviesContainer = document.querySelector('.categoriesPreview-container .categoriesPreview-list');
+
+        //Se crean los componentes que forman parte de cada uno de los elementos de la lista y se le agregan los atributos que requieren, revisar index.html
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+        
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', category.id);
+        
+        const categoryTitleText = document.createTextNode(category.name);  //se crea el texto para el h3.
+
+        //se agregan cada un de los componentes como hijos según la herarquia para se mostrados en la pagina web.
+        categoryTitle.appendChild(categoryTitleText); 
+        categoryContainer.appendChild(categoryTitle);
+        categoriesMoviesContainer.appendChild(categoryContainer);
+    });
+}
+
 getTrendingMovies(); //se llama esta función para mostrar las peliculas en tendencia.
+getCategoriesMovies();  //se llama esta función para mostrar las categorias de peliculas.
